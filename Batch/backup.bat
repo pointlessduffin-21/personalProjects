1beaa3b71c4494aa503eb188ce717d4d583d0a01
pointlessduffin-21 <francisroelabarca12@gmail.com>
2023-12-15 14:53:31 +0800

    added backup.bat for ddi bdse-0923

---
+++ Batch/backup.bat
@@ -0,0 +1,15 @@
+HO OFF
+CLS
+REM SET VARIABLES VALUES
+REM
+REM SET MySQLDir=C:\xampp\mysql\bin
+REM SET %dbName%=manzaneque_limited
+REM SET BackupFileName=%Database%%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%%TIME:~0,2%%TIME:~3,2%.sql
+REM SET BackupFolderPath=D:/Backups/
+REM
+REM IF NOT EXIST %BackupFolderPath% mkdir %BackupFolderPath%
+REM
+REM ECHO DB Backup started...
+REM cd %MySQLDir%
+REM mysqldump -u root -p %dbName% > "%BackupFolderPath%%BackupFileName%"
+REM ECHO DB Backup completed... 
