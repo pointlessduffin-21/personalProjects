be5998d508fde0ffd057f5ba3dd16660f06a84b5
pointlessduffin-21 <francisroelabarca12@gmail.com>
2023-12-15 14:59:21 +0800

    added backup.bat

--- Batch/backup.bat
+++ Batch/backup.bat
@@ -1,15 +1,15 @@
-HO OFF
+@ECHO OFF
 CLS
 REM SET VARIABLES VALUES
-REM
-REM SET MySQLDir=C:\xampp\mysql\bin
-REM SET %dbName%=manzaneque_limited
-REM SET BackupFileName=%Database%%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%%TIME:~0,2%%TIME:~3,2%.sql
-REM SET BackupFolderPath=D:/Backups/
-REM
-REM IF NOT EXIST %BackupFolderPath% mkdir %BackupFolderPath%
-REM
-REM ECHO DB Backup started...
-REM cd %MySQLDir%
-REM mysqldump -u root -p %dbName% > "%BackupFolderPath%%BackupFileName%"
-REM ECHO DB Backup completed... 
+
+SET MySQLDir=C:\xampp\mysql\bin
+SET %dbName%=manzaneque_limited
+SET BackupFileName=%Database%%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%%TIME:~0,2%%TIME:~3,2%.sql
+SET BackupFolderPath=D:/Backups/
+
+IF NOT EXIST %BackupFolderPath% mkdir %BackupFolderPath%
+
+ECHO DB Backup started...
+cd %MySQLDir%
+mysqldump -u root -p %dbName% > "%BackupFolderPath%%BackupFileName%"
+ECHO DB Backup completed... 
