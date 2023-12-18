5fdc3b5380a8d484f661ba7188ec1b617ab9b212
pointlessduffin-21 <francisroelabarca12@gmail.com>
2023-12-18 21:16:29 +0800

    Added a switch case for conditionals and more rng

--- Java/number-generator/src/main/java/xyz/yeems214/Main.java
+++ Java/number-generator/src/main/java/xyz/yeems214/Main.java
@@ -1,33 +1,54 @@
 package xyz.yeems214;
 
+import xyz.yeems214.PCSO.sixFiftyEight;
+import xyz.yeems214.PCSO.sixFortyNine;
 import xyz.yeems214.PCSO.sixFortyTwo;
 
 import java.util.Scanner;
 
 public class Main {
 
-    public static void text(String splashScreen, String items) {
+    public static void text(String splashScreen, String item1, String item2, String item3, String item4) {
         System.out.println(splashScreen);
-        System.out.println(items);
+        System.out.println(item1);
+        System.out.println(item2);
+        System.out.println(item3);
+        System.out.println(item4);
     }
 
     public static void Scanner() {
-        Scanner scanner = new Scanner(System.in);
-        int choice = scanner.nextInt();
-        switch (choice) {
-            case 1:
-                sixFortyTwo.main(null);
-                break;
-            default:
-                System.out.println("Invalid choice!");
-                Scanner();
+        try {
+            Scanner scanner = new Scanner(System.in);
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    sixFortyTwo.main(null);
+                    main(null);
+                case 2:
+                    sixFortyNine.main(null);
+                    main(null);
+                case 3:
+                    sixFiftyEight.main(null);
+                    main(null);
+                case 0:
+                    break;
+                default:
+                    System.out.println("\nInvalid choice!");
+                    Scanner();
+            }
+            scanner.close();
+        } catch (Exception e) {
+            System.out.println("\nApplication Quit!");
+//            System.out.println("Error: " + e.getMessage());
         }
-        scanner.close();
     }
     public static void main(String[] args) {
-        String splashScreen = "Welcome to the Random Number Generator Software!";
-        String items = "Press 1 for 6/42";
-        text(splashScreen, items);
+        String splashScreen = "\n\nWelcome to the Random Number Generator Software!";
+        String item1 = "Press 1 for 6/42";
+        String item2 = "Press 2 for 6/49";
+        String item3 = "Press 3 for 6/58";
+        String item4 = "Press 0 to quit";
+        text(splashScreen, item1, item2, item3, item4);
         Scanner();
     }
 }
--- Java/number-generator/src/main/java/xyz/yeems214/PCSO/sixFiftyEight.java
+++ Java/number-generator/src/main/java/xyz/yeems214/PCSO/sixFiftyEight.java
@@ -2,14 +2,14 @@ package xyz.yeems214.PCSO;
 
 import java.util.Random;
 
-public class sixFortyTwo {
+public class sixFiftyEight {
     public static void main(String[] args) {
         Random random = new Random();
         int[] array = new int[6];
 
         for (int i = 0; i < array.length; i++) {
             // Generate valid two-digit number within 1-42 range
-            int num = 1 + random.nextInt(42);
+            int num = 1 + random.nextInt(58);
             // Extract and store individual digits
             int firstDigit = num / 10;
             int secondDigit = num % 10;
@@ -18,7 +18,7 @@ public class sixFortyTwo {
             array[i] = Integer.parseInt(numberString);
         }
 
-        System.out.println("Random 6-number array (6/42):");
+        System.out.println("Random 6-number array (6/58):");
         for (int number : array) {
             System.out.print(number + " ");
         }
